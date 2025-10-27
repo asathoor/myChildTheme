9bb4a0cc41e703b3a8f7636d168e6426aa7a4ffe
asathoor <asathoor@gmail.com>
2025-10-27 20:19:23 +0100

    functions.php rettet til

--- functions.php
+++ functions.php
@@ -1,12 +1,10 @@
 <?php
-// parent styles
-add_action( 'wp_enqueue_scripts', 'petj_barnet_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'petj_enqueue_styles' );
 
-function petj_barnet_enqueue_styles() {
-	// forældretemaets css
+function petj_enqueue_styles() {
+	// parent theme css
 	wp_enqueue_style( 'petj-tt5-parent-style',  get_parent_theme_file_uri( 'style.css' ) );
 
-	// barnetemaets css
+	// child theme css
 	wp_enqueue_style( 'petj_barnet_enqueue_styles', get_stylesheet_uri() );
-
 }
